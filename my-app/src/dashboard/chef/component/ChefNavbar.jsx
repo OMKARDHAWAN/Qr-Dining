497d50cd206812b7914452f69f71a4c7acef7744
OMKARDHAWAN <dhawanomkar80@gmail.com>
2026-06-21 18:25:24 +0530

    commit by omkar dhawan

--- my-app/src/dashboard/chef/component/ChefNavbar.jsx
+++ my-app/src/dashboard/chef/component/ChefNavbar.jsx
@@ -2,7 +2,7 @@ export default function ChefNavbar(){
     return(
         <>
         <div className="border border-blue-500">
-        <h1>this is chef Navbar</h1>
+        <h1 className="#BB2409">this is chef Navbar</h1>
         </div>
         </>
     )
