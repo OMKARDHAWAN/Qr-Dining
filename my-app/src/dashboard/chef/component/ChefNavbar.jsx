a370c8e2bb14d6f421306d5d080621b24b77efac
manvendra9691 <kushwahamanvendra29@gmail.com>
2026-08-01 08:11:47 +0530

    update by Manvendra: added backend migrations, AppDbContext and frontend chef components & CSS files

---
+++ my-app/src/dashboard/chef/component/ChefNavbar.jsx
@@ -0,0 +1,9 @@
+export default function ChefNavbar(){
+    return(
+        <>
+        <div className="border border-blue-500">
+        <h1 className="#BB2409">this is chef Navbar</h1>
+        </div>
+        </>
+    )
+}
