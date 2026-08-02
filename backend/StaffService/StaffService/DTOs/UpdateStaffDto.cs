5c3a0c16ee40d3c082cdf9cf7fb7d1fb6dad26ca
nitinmishra15 <nitinmishra1500@gmail.com>
2026-08-03 00:13:02 +0530

    Updated StaffService backend

---
+++ backend/StaffService/StaffService/DTOs/UpdateStaffDto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace StaffService.DTOs
+{
+    public class UpdateStaffDto
+    {
+        public string Name { get; set; }
+
+        public string Role { get; set; }
+
+        public string Department { get; set; }
+
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Status { get; set; }
+
+        public IFormFile? Image { get; set; }
+    }
+}
